36a799688cc70c2647de82949e6f1676fcc2b5be
cwill83247 <breakthrumusic@hotmail.com>
2022-11-26 16:13:33 +0000

    Add: Adding the inital registration page

--- app.py
+++ app.py
@@ -25,6 +25,31 @@ def homepage():
     return render_template("index.html")
 
 
+#register
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(                        
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:                                               
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {                                                           
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))    
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()            
+        flash("Thank you for registering!")
+        return redirect(url_for("homepage", username=session["user"]))            
+    
+    return render_template("register.html")
+
 #tell app how and when to run
 if __name__ == "__main__":                              
     app.run(host=os.environ.get("IP"),
